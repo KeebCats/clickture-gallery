783e3df16597b3af20ca2e8b2ca19c635f3bca83
duffpop <45819703+duffpop@users.noreply.github.com>
2023-02-13 03:34:08 +0000

    added relevant info

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,4 @@
-import Document, { Head, Html, Main, NextScript } from 'next/document'
+import Document, { Head, Html, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
   render() {
@@ -8,19 +8,25 @@ class MyDocument extends Document {
           <link rel="icon" href="/favicon.ico" />
           <meta
             name="description"
-            content="See pictures from Next.js Conf and the After Party."
+            content="Gina by KeebCats - A gallery of pictures of the Gina Macro-Numpad."
           />
-          <meta property="og:site_name" content="nextjsconf-pics.vercel.app" />
+          <meta property="og:site_name" content="gallery.gina.keebcats.co.uk" />
           <meta
             property="og:description"
-            content="See pictures from Next.js Conf and the After Party."
+            content="Gina by KeebCats - A gallery of pictures of the Gina Macro-Numpad."
           />
-          <meta property="og:title" content="Next.js Conf 2022 Pictures" />
+          <meta
+            property="og:title"
+            content="Gina Macro-Numpad by KeebCats - Clickture Gallery"
+          />
           <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:title" content="Next.js Conf 2022 Pictures" />
+          <meta
+            name="twitter:title"
+            content="Gina Macro-Numpad by KeebCats - Clickture Gallery"
+          />
           <meta
             name="twitter:description"
-            content="See pictures from Next.js Conf and the After Party."
+            content="See pictures for the Gina Macro-Numpad by KeebCats."
           />
         </Head>
         <body className="bg-black antialiased">
@@ -28,8 +34,8 @@ class MyDocument extends Document {
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
 
-export default MyDocument
+export default MyDocument;
